e69a50f47640964be1ec45b2ab14aeff5f8ff06d
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2023-06-22 15:31:58 +0530

    before path testing

--- new.rs
+++ new.rs
@@ -2,7 +2,7 @@
 
 #[ink::contract]
 mod escrow {
-    use ink::storage::Mapping;
+    use ink::{storage::Mapping};
     use openbrush::traits::String;
     // use openbrush::traits::Balance;
 
@@ -64,10 +64,10 @@ mod escrow {
     #[derive(Default)]
     pub struct Escrow {
         current_audit_id: u32,
-        audit_id_to_payment_info: Mapping<u32, PaymentInfo>,
+        pub audit_id_to_payment_info: Mapping<u32, PaymentInfo>,
         // for the transfers of funds to the arbiters, we will simply send the money to arbiterprovider in the withdrawal, and they are supposed to deal with the rest.
-        audit_id_to_time_increase_request: ink::storage::Mapping<u32, IncreaseRequest>,
-        audit_id_to_ipfs_hash: ink::storage::Mapping<u32, String>,
+        pub audit_id_to_time_increase_request: ink::storage::Mapping<u32, IncreaseRequest>,
+        pub audit_id_to_ipfs_hash: ink::storage::Mapping<u32, String>,
     }
 
     impl Escrow {
@@ -170,25 +170,41 @@ mod escrow {
 
         #[ink(message)]
         pub fn approve_additional_time(&mut self, id: u32) -> bool {
+            println!("Entered the function approve_additional_time");
             if self.get_payment_info(&id).unwrap().patron == self.env().caller() {
                 let haircut = self
                     .query_timeincreaserequest(id)
                     .unwrap()
                     .haircut_percentage;
                 if haircut < 100 {
+                    println!("Entered the haircut mark");
                     let new_deadline = self.query_timeincreaserequest(id).unwrap().newdeadline;
+
+
+                    let mut payment_info = self.audit_id_to_payment_info.get(id).unwrap();
+            // Update the value in storage
+                payment_info.value = payment_info.value*(100 - haircut) / 100;
+            // Update the deadline in storage
+                payment_info.deadline = new_deadline;
+            // Set the updated payment_info back to storage
+                self.audit_id_to_payment_info.insert(id, &payment_info);
                     //if *= doesn't work, use "self.get_payment_info(&id).unwrap().value *"
-                    self.audit_id_to_payment_info.get(id).unwrap().value *= (100 - haircut) / 100;
-                    self.audit_id_to_payment_info.get(id).unwrap().deadline = new_deadline;
+
+                    // self.audit_id_to_payment_info.get(id).unwrap().value *= (100 - haircut) / 100;
+                    // println!("Entered the line just before.");
+                    // self.audit_id_to_payment_info.get(id).unwrap().deadline = new_deadline;
                     self.env().emit_event(AuditInfoUpdated {
                         id: Some(id),
                         payment_info: Some(self.audit_id_to_payment_info.get(id).unwrap()),
                         updated_by: Some(self.get_payment_info(&id).unwrap().patron),
                     });
+                    println!("we can be sure that approve_extended deadline will return true");
                     return true;
                 }
+                println!(" Function is returning the internal false");
                 return false;
             }
+            println!(" Function is returning the external false");
             false
         }
 
@@ -238,12 +254,22 @@ mod escrow {
             }
         }
 
+//to_do 
+// in every function where haircut is mentioned, return the rest of the amount to the patron. 
+//complete this task after integrating psp22/erc20.
         #[ink(message)]
-        pub fn arbiters_extend_deadline(&self, _id: u32, newdeadline: u64, haircut: Balance) -> bool {
+        pub fn arbiters_extend_deadline(&mut self, _id: u32, new_deadline: u64, haircut: Balance) -> bool {
             //checking for the haircut to be lesser than 10% and new deadline to be at least more than 1 day.
-            if haircut < 10 && newdeadline > self.env().block_timestamp() + 86400 {
-                self.audit_id_to_payment_info.get(_id).unwrap().deadline = newdeadline;
-                self.audit_id_to_payment_info.get(_id).unwrap().value *= (100-haircut)/100;
+            if haircut < 10 && new_deadline > self.env().block_timestamp() + 86400 {
+                println!("At least the code got into the if case of arbiters_extend_deadline");
+
+                let mut payment_info = self.audit_id_to_payment_info.get(_id).unwrap();
+                // Update the value in storage
+                payment_info.value = payment_info.value*(100 - haircut) / 100;
+                // Update the deadline in storage
+                payment_info.deadline = new_deadline;
+                // Set the updated payment_info back to storage
+                self.audit_id_to_payment_info.insert(_id, &payment_info);
                 self.env().emit_event(AuditInfoUpdated {
                     id: Some(_id),
                     payment_info: Some(self.audit_id_to_payment_info.get(_id).unwrap()),
@@ -253,5 +279,100 @@ mod escrow {
             }
             false
         }
+    }
+}
+
+
+
+#[cfg(test)]
+mod test_cases {
+    use ink::primitives::AccountId;
+
+    use super ::*;
+    #[cfg(feature = "ink-experimental-engine")]
+    use crate::digital_certificate::digital_certificate;
+    fn random_account_id() -> AccountId {
+        AccountId::from([0x42;32])
+    }
+
+    #[test]
+    fn test_case_access_current_audit_id() {
+        let accounts = 
+        ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        ink::env::test::set_callee::<ink::env::DefaultEnvironment>(accounts.bob);
+        let contract = escrow::Escrow::new();
+        assert_eq!(contract.get_current_audit_id(),0);
+        println!("I'm here.");
+    }
+    #[test]
+    fn test_case_make_new_audit() {
+        let accounts = 
+        ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        ink::env::test::set_callee::<ink::env::DefaultEnvironment>(accounts.bob);
+        let mut contract = escrow::Escrow::new();
+        let value: openbrush::traits::Balance = 100;
+        let auditor = accounts.bob;
+        let arbiter_provider = accounts.charlie;
+        let deadline: u64 = 1688329909;
+
+        contract.create_new_payment(value, auditor, arbiter_provider, deadline);
+        assert_eq!(contract.get_paymentinfo(contract.get_current_audit_id()-1).unwrap().deadline, deadline);
+    }
+
+    #[test]
+    fn test_case_request_change_deadline() {
+        let accounts = 
+        ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        ink::env::test::set_callee::<ink::env::DefaultEnvironment>(accounts.bob);
+        let mut contract = escrow::Escrow::new();
+        let value: openbrush::traits::Balance = 100;
+        let auditor = accounts.bob;
+        let arbiter_provider = accounts.charlie;
+        let deadline: u64 = 1688329909;
+        let haircut: openbrush::traits::Balance = 5;
+
+        contract.create_new_payment(value, auditor, arbiter_provider, deadline);
+        //line for checking if our payment info's audit id has been assigned.
+        assert_eq!(contract.get_paymentinfo(contract.get_current_audit_id()-1).unwrap().value, 100);
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.bob);
+        contract.request_additional_time(0, 1698329909, haircut);
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        assert_eq!(contract.approve_additional_time(0), true);
+        // println!(contract.get_payment_info(0).unwrap().deadline);
+        //throwing string error.
+        assert_eq!(contract.audit_id_to_payment_info.get(0).unwrap().deadline, 1698329909);
+        assert_eq!(contract.audit_id_to_payment_info.get(0).unwrap().value, value * (100-haircut)/100);
+    }
+
+    #[test]
+    fn test_case_arbiter_extends_deadline() {
+        let accounts = 
+        ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        ink::env::test::set_callee::<ink::env::DefaultEnvironment>(accounts.bob);
+        let mut contract = escrow::Escrow::new();
+        let value: openbrush::traits::Balance = 100;
+        let auditor = accounts.bob;
+        let arbiter_provider = accounts.charlie;
+        let deadline: u64 = 1687329909;
+        let _haircut: openbrush::traits::Balance = 5;
+
+        contract.create_new_payment(value, auditor, arbiter_provider, deadline);
+
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.bob);
+        let ipfs_hash= "blah-blah";
+        println!("printing my ipfs hash here {}", ipfs_hash);
+        assert_eq!(contract.mark_completed(0, ipfs_hash.into()), true);
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        contract.assess_audit(0, false);
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.charlie);
+        let _newtime:u64= 1688329909;
+        let _arbiters_haircut = 5;
+        contract.arbiters_extend_deadline(0, _newtime, _arbiters_haircut);
+        assert_eq!(contract.audit_id_to_payment_info.get(0).unwrap().deadline, _newtime);
+        println!("At the end of the testcase.");
     }
 }
