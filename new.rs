319abbe521584d799d9203e70560c825a8342e69
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2023-06-22 18:40:41 +0530

    covered main path t

--- new.rs
+++ new.rs
@@ -233,12 +233,17 @@ mod escrow {
             if caller == audit_patron || caller == audit_arbiterprovider {
                 if answer && !self.audit_id_to_payment_info.get(_id).unwrap().completed {
                     if caller == audit_patron {
-                        self.audit_id_to_payment_info.get(_id).unwrap().completed = answer;
+                        let mut payment_info = self.audit_id_to_payment_info.get(_id).unwrap();
+                        payment_info.completed = true;
+                        self.audit_id_to_payment_info.insert(_id, &payment_info);
+
                     // to_do
                     //transferring tokens 98% to the auditor,
                     //transferring tokens 2% to the arbiter_provider for being there.
                     } else {
-                        self.audit_id_to_payment_info.get(_id).unwrap().completed = answer;
+                        let mut payment_info = self.audit_id_to_payment_info.get(_id).unwrap();
+                        payment_info.completed = true;
+                        self.audit_id_to_payment_info.insert(_id, &payment_info);
                         //to_do
                         //transfer 85% to the auditor, 15 for arbiterprovider and 5 arbiters.
                     }
@@ -256,7 +261,7 @@ mod escrow {
 
 //to_do 
 // in every function where haircut is mentioned, return the rest of the amount to the patron. 
-//complete this task after integrating psp22/erc20.
+//complete this task after integrating psp22/erc20
         #[ink(message)]
         pub fn arbiters_extend_deadline(&mut self, _id: u32, new_deadline: u64, haircut: Balance) -> bool {
             //checking for the haircut to be lesser than 10% and new deadline to be at least more than 1 day.
@@ -374,5 +379,103 @@ mod test_cases {
         contract.arbiters_extend_deadline(0, _newtime, _arbiters_haircut);
         assert_eq!(contract.audit_id_to_payment_info.get(0).unwrap().deadline, _newtime);
         println!("At the end of the testcase.");
+    }
+
+
+    #[test]
+    fn test_case_p1() {
+        //p1 is I->II.a->III.a 
+        // so patron creates the auditID, 
+        //the auditor will submit,
+        //patron accepts, 
+
+        let accounts = 
+        ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        ink::env::test::set_callee::<ink::env::DefaultEnvironment>(accounts.bob);
+        let mut contract = escrow::Escrow::new();
+        let value: openbrush::traits::Balance = 100;
+        let auditor = accounts.bob;
+        let arbiter_provider = accounts.charlie;
+        let deadline: u64 = 1687329909;
+        let _haircut: openbrush::traits::Balance = 5;
+        //I
+        contract.create_new_payment(value, auditor, arbiter_provider, deadline);
+        //II.a
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.bob);
+        let ipfs_hash = "yippiee";
+        assert_eq!(contract.mark_completed(0, ipfs_hash.into()), true);
+        //III.a
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        contract.assess_audit(0, true);
+        assert_eq!(contract.audit_id_to_payment_info.get(0).unwrap().completed, true);
+    }
+
+    #[test]
+    fn test_case_p2 () {
+        //I->II.a->III.b.1
+        //p1 is I->II.a->III.a 
+        // so patron creates the auditID, 
+        //the auditor will submit,
+        //patron does not accept, 
+        //arbiter_provider will extend the deadline with/without haircut off the value.
+        
+        let accounts = 
+        ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        ink::env::test::set_callee::<ink::env::DefaultEnvironment>(accounts.bob);
+        let mut contract = escrow::Escrow::new();
+        let value: openbrush::traits::Balance = 100;
+        let auditor = accounts.bob;
+        let arbiter_provider = accounts.charlie;
+        let deadline: u64 = 1687329909;
+        let _haircut: openbrush::traits::Balance = 5;
+        //I
+        contract.create_new_payment(value, auditor, arbiter_provider, deadline);
+        //II.a
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.bob);
+        let ipfs_hash = "yippiee";
+        assert_eq!(contract.mark_completed(0, ipfs_hash.into()), true);
+        //III.b.1
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        contract.assess_audit(0, false);
+        assert_eq!(contract.audit_id_to_payment_info.get(0).unwrap().completed, false);
+        //arbiter provider increasing the deadline.
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.charlie);
+        let new_deadline:u64 = 1688329909;
+        assert_eq!(contract.arbiters_extend_deadline(0,new_deadline , 0), true);
+        //II.a
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.bob);
+        let ipfs_hash = "new report link";
+        assert_eq!(contract.mark_completed(0, ipfs_hash.into()), true);
+        //III.a
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        contract.assess_audit(0, true);
+        assert_eq!(contract.audit_id_to_payment_info.get(0).unwrap().completed, true);
+
+    }
+
+    #[test]
+    fn test_case_p3 () {
+        //I->II.a->III.b.1
+        //p1 is I->II.a->III.a 
+        // so patron creates the auditID, 
+        //the auditor will submit,
+        //patron does not accept, 
+        //arbiter_provider will extend the deadline with/without haircut off the value.
+        
+        let accounts = 
+        ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.alice);
+        ink::env::test::set_callee::<ink::env::DefaultEnvironment>(accounts.bob);
+        let mut contract = escrow::Escrow::new();
+        let value: openbrush::traits::Balance = 100;
+        let auditor = accounts.bob;
+        let arbiter_provider = accounts.charlie;
+        let deadline: u64 = 1687329909;
+        let _haircut: openbrush::traits::Balance = 5;
+        //I
+        contract.create_new_payment(value, auditor, arbiter_provider, deadline);
+        
     }
 }
